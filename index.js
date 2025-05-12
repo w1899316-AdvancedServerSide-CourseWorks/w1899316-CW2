0a8165498965ea38777088a8776e06335cd2c8a5
Chamith <chamith.eos@gmail.com>
2025-05-12 11:54:21 +0530

    add the travel sales url

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'http://localhost:3003'],
   credentials: true
 }));
 app.use(express.json({ limit: '100mb' }));
